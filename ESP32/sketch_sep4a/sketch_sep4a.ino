f3045633d0f244f7bb96346c403001558e7a065f
Mark Kenneth Nudo <kennethnudo27@gmail.com>
2025-09-05 11:24:45 +0800

    added MQTT Server fields in the WiFi captive portal on ESP32

--- ESP32/sketch_sep4a/sketch_sep4a.ino
+++ ESP32/sketch_sep4a/sketch_sep4a.ino
@@ -1,37 +1,83 @@
-#include <WiFiManager.h>
 #include <WiFi.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+#include <Preferences.h>
 
-#define RESET_PIN 0   // BOOT button
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-WiFiManager wm;
+Preferences preferences;
+
+char mqtt_server[40] = "raspberrypi.local";
+char mqtt_port[6]   = "1883";
+
+void saveConfig() {
+  preferences.begin("mqtt", false);
+  preferences.putString("server", mqtt_server);
+  preferences.putString("port", mqtt_port);
+  preferences.end();
+}
+
+void loadConfig() {
+  preferences.begin("mqtt", true);
+  String server = preferences.getString("server", "raspberrypi.local");
+  String port   = preferences.getString("port", "1883");
+  preferences.end();
+
+  server.toCharArray(mqtt_server, sizeof(mqtt_server));
+  port.toCharArray(mqtt_port, sizeof(mqtt_port));
+}
+
+void setupWifi() {
+  WiFiManager wm;
+
+  WiFiManagerParameter custom_mqtt_server("server", "MQTT Server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "MQTT Port", mqtt_port, 6);
+
+  wm.addParameter(&custom_mqtt_server);
+  wm.addParameter(&custom_mqtt_port);
+
+  if (!wm.autoConnect("ESP32_AP")) {
+    Serial.println("Failed to connect, restarting...");
+    delay(3000);
+    ESP.restart();
+  }
+
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  saveConfig();
+
+  Serial.println("WiFi connected!");
+  Serial.print("MQTT Server: "); Serial.println(mqtt_server);
+  Serial.print("MQTT Port: "); Serial.println(mqtt_port);
+}
+
+void reconnectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      client.publish("test/topic", "Hello from ESP32!");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 5s");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(RESET_PIN, INPUT_PULLUP);
+  loadConfig();       // Load previously saved settings
+  setupWifi();        // Start WiFi + captive portal if needed
 
-  if (digitalRead(RESET_PIN) == LOW) {
-    Serial.println("resetting WiFi settings...");
-    wm.resetSettings();
-    delay(1000);
-  }
-
-  if (!wm.autoConnect("ESP32_AP", "12345678")) {
-    Serial.println("Failed to connect and hit timeout, restarting...");
-    ESP.restart();
-  }
-
-  Serial.println("Connected to WiFi!");
-  Serial.print("ESP32 IP Address: ");
-  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, atoi(mqtt_port));
 }
 
 void loop() {
-  if (digitalRead(RESET_PIN) == LOW) {
-    Serial.println("Button pressed → resetting WiFi and restarting...");
-    wm.resetSettings();
-    delay(1000);
-    ESP.restart();
+  if (!client.connected()) {
+    reconnectMQTT();
   }
-
-  // Your normal logic here (MQTT, sensors, etc.)
+  client.loop();
 }
